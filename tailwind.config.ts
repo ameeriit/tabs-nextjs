c9cc90fa335f3df618c37f0967af5603212196da
ameeriit <amritpokharel51@gmail.com>
2024-02-04 10:17:14 +0545

    Project Setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 const config: Config = {
   content: [
@@ -7,11 +8,29 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontSize: {
+      base: "1rem", // 16px
+      md: "1.125rem", // 18px
+      lg: "1.25rem", // 20px
+      xl: "1.5rem", // 24px
+      "2xl": "2rem", // 32px
+      "3xl": "2.5rem", // 40px
+      "4xl": "3rem", // 48px
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: {
+          50: "#FFE58C",
+          100: "#FFD540",
+        },
+        secondary: "#F15A29",
+        tertiary: "#EBECF5",
+        shadow: "#3C479D",
+        gray: "#CCCCCC",
+        lightGray: "#f7f7f7f7",
+      },
+      fontFamily: {
+        poppins: ["var(--font-poppins)", ...fontFamily.sans],
       },
     },
   },
